cc2892fdd7bd0569182edcdd0594cb6e1b5d49ee
enforcer007 <akhilgodvsdemon@gmail.com>
2022-05-09 14:33:42 +0530

    With Answers

--- src/defaults.rs
+++ src/defaults.rs
@@ -1,1 +1,4 @@
-pub const URL: &str = "https://eyodmpf7zi.execute-api.us-east-1.amazonaws.com/prod/public/question";
+pub const QUESTION_URL: &str =
+    "https://eyodmpf7zi.execute-api.us-east-1.amazonaws.com/prod/public/question";
+pub const ANSWER_URL: &str =
+    "https://5fygdsrbgl.execute-api.us-east-1.amazonaws.com/prod/public/question";
--- src/today_question.rs
+++ src/today_question.rs
@@ -3,6 +3,7 @@ use crate::write_file::ToFileContent;
 use chrono::prelude::*;
 use reqwest;
 use serde::{de, Deserialize, Deserializer};
+use std::{collections::HashMap, fmt::format};
 
 fn naive_date_time_from_str<'de, D>(deserializer: D) -> Result<NaiveDate, D::Error>
 where
@@ -19,6 +20,12 @@ pub struct BnomialRespContent {
     title: String,
     content: String,
     choices: Vec<String>,
+    #[serde(default)]
+    answer: String,
+    #[serde(default)]
+    explanation: String,
+    #[serde(default)]
+    references: String,
 }
 
 #[derive(Deserialize)]
@@ -34,14 +41,53 @@ impl ToFileContent for BnomialRespContent {
             .map(|x| "- ".to_owned() + x)
             .reduce(|x, y| [x, y].join("\n"))
             .unwrap();
+        let answer = format!(
+            "<details><summary>CLICK ME</summary><p>{}</p></details>",
+            self.answer
+        );
         format!(
-            "## Date - {}\n\n\n## Title - {}\n\n\n### **Question** :\n\n{}\n\n\n### **Choices** :\n\n{}\n\n-----------------------\n\n",
-            self.date, self.title, self.content, choices
+            "## Date - {}\n\n\n## Title - {}\n\n\n### **Question** :\n\n{}\n\n\n### **Choices** :\n\n{}\n\n\n### **Answer** :\n\n{}\n\n\n### **Explaination** :\n\n{}\n\n\n### **References**: \n\n{}\n\n-----------------------\n\n",
+            self.date, self.title, self.content, choices, answer, self.explanation, self.references
         )
     }
 }
 
+fn bnomial_rand_answer(n_choices: usize) -> String {
+    let mut answer = "0".repeat(n_choices - 1);
+    answer.push('1');
+    answer
+}
+
 pub fn today_question() -> Result<BnomialRespContent, Box<dyn std::error::Error>> {
-    let response = reqwest::blocking::get(defaults::URL)?.json::<BnomialResp>()?;
+    let client = reqwest::blocking::Client::new();
+    let response_content = client
+        .get(defaults::QUESTION_URL)
+        .send()?
+        .json::<BnomialResp>()?
+        .question;
+
+    let random_answer = bnomial_rand_answer(response_content.choices.len());
+
+    let mut map = HashMap::new();
+    map.insert("answer", random_answer);
+
+    let response = client
+        .post(defaults::ANSWER_URL)
+        .json(&map)
+        .send()?
+        .json::<BnomialResp>()?;
     Ok(response.question)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_post() {
+        let mut map = HashMap::new();
+        map.insert("answer", "0000");
+        let client = reqwest::blocking::Client::new();
+        let _response = client.post(defaults::ANSWER_URL).json(&map).send().unwrap();
+    }
 }
